e908537ca14889f58c58899925fe7420bfe23790
DAYGoodTime <ken570015@163.com>
2024-07-13 15:23:37 +0800

    log:add slf4j impl for kookbc logging

--- src/main/java/com/day/mctokook/ServerProxy.java
+++ src/main/java/com/day/mctokook/ServerProxy.java
@@ -1,26 +1,30 @@
 package com.day.mctokook;
 
-import com.day.mctokook.commands.kook.CommandInit;
-import com.day.mctokook.commands.mc.ReInitCommand;
-import com.day.mctokook.listener.KookListener;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
 import net.minecraft.command.ICommandManager;
 import net.minecraft.command.ServerCommandManager;
 import net.minecraft.server.MinecraftServer;
+
+import org.slf4j.LoggerFactory;
+
+import com.day.mctokook.commands.kook.CommandInit;
+import com.day.mctokook.commands.mc.ReInitCommand;
+import com.day.mctokook.listener.KookListener;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import snw.jkook.JKook;
 import snw.jkook.config.file.YamlConfiguration;
 import snw.jkook.entity.channel.TextChannel;
 import snw.kookbc.impl.CoreImpl;
 import snw.kookbc.impl.KBCClient;
 import snw.kookbc.impl.plugin.InternalPlugin;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
 
 public class ServerProxy extends CommonProxy {
 
@@ -47,7 +51,7 @@ public class ServerProxy extends CommonProxy {
         YamlConfiguration config = new YamlConfiguration();
 
         config.set("mode", "websocket");
-        CoreImpl core = new CoreImpl();
+        CoreImpl core = new CoreImpl(LoggerFactory.getLogger("KooKBC"));
         JKook.setCore(core);
 
         // 读取配置拿必要的东西
@@ -66,15 +70,15 @@ public class ServerProxy extends CommonProxy {
             }
         }
         connectKBC(core, config, bot_token, modInstance, channel_ID);
-        //registry server command
+        // registry server command
         MinecraftServer server = MinecraftServer.getServer();
-        if (server == null || server.getCommandManager()==null) {
-            McToKook.LOG.error("无法注册命名，是否在服务端环境内?");
+        if (server == null || server.getCommandManager() == null) {
+            McToKook.LOG.error("无法注册命令，是否在服务端环境内?");
             return;
         }
         ICommandManager cm = server.getCommandManager();
-        if(cm instanceof ServerCommandManager) {
-            ServerCommandManager scm = (ServerCommandManager)cm;
+        if (cm instanceof ServerCommandManager) {
+            ServerCommandManager scm = (ServerCommandManager) cm;
             scm.registerCommand(new ReInitCommand());
         }
     }
@@ -84,7 +88,7 @@ public class ServerProxy extends CommonProxy {
         McToKook.kbcClient = new KBCClient(core, config, null, bot_token);
 
         McToKook.kbcClient.start();
-        //主动尝试获取channel,如果任何
+        // 主动尝试获取channel,如果任何
         TextChannel channel = (TextChannel) McToKook.kbcClient.getCore()
             .getHttpAPI()
             .getChannel(channel_ID);
@@ -96,6 +100,7 @@ public class ServerProxy extends CommonProxy {
         // 注册KOOK指令
         InternalPlugin plugin = McToKook.kbcClient.getInternalPlugin();
         CommandInit.InitKookCommand(plugin);
+        //
     }
 
     // register server commands in this event handler (Remove if not needed)
